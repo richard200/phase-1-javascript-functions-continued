62b8fd21f77a1c05ed131eac7bb5bc2095c83911
richard200 <richmn200@gmail.com>
2022-11-24 11:30:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(def = "roller-skate"){
+    return `This Saturday, I want to ${def}!`
+   }
+
+saturdayFun("bathe my dog")
+
+function mondayWork(def = "go to the office"){
+    return `This Monday, I will ${def}.`
+   }
+
+mondayWork("work from home")
+
+function wrapAdjective(symb){
+   return function wrapper(mess){
+        return `You are ${symb}${mess}${symb}!`
+    }
+}
+    
+wrapAdjective()
